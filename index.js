0797ad33e5c53a9cf4f8f74951f4a1913f8a2936
uetrozi <charlotte_keuer@posteo.de>
2023-11-22 17:51:05 +0100

    git add . 2

--- index.js
+++ index.js
@@ -11,27 +11,54 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
   // try {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  );
   if (response.ok) {
     const data = await response.json();
-
+    const currentPage = page;
+    maxPage = data.info.pages;
     data.results.forEach((character) => {
-      console.log(character)
       const carCard = createCharacterCard(character);
       cardContainer.append(carCard);
     });
   } else {
     console.log("Bad Response");
   }
+  updateNavigation();
   // } catch (error) {
   //   console.log("An Error occured", error);
   // }
 }
 
-fetchCharacters();
+function updateNavigation() {
+  pagination.textContent = `${page} / ${maxPage}`;
+  prevButton.disabled = page == 1;
+  nextButton.disabled = page == maxPage;
+}
+
+fetchCharacters().then(updateNavigation);
+
+nextButton.addEventListener("click", () => {
+  cardContainer.innerHTML = "";
+  page++;
+  if (page > maxPage) {
+    page = maxPage;
+  }
+  fetchCharacters();
+});
+
+prevButton.addEventListener("click", () => {
+  cardContainer.innerHTML = "";
+  page--;
+  if (page < 1) {
+    page = 1;
+  }
+  fetchCharacters();
+});
