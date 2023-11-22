5fd7fb9152c4a88adeda2f1dfa516525a317c955
uetrozi <charlotte_keuer@posteo.de>
2023-11-22 19:11:46 +0100

    create pagination function

---
+++ components/nav-pagination/nav-pagination.js
@@ -0,0 +1,7 @@
+export function createPagination() {
+    const pagination = document.createElement("span");
+    pagination.classList.add("navigation__pagination");
+    pagination.setAttribute("data-js", "pagination");
+    pagination.textContent = `${page} / ${maxPage}`;
+    return pagination;
+  }
