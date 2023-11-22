7b089ddb235a97cd2179766bbe9f454c2c9ec77d
uetrozi <charlotte_keuer@posteo.de>
2023-11-22 19:14:51 +0100

    add nav functionality to buttons

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -3,14 +3,29 @@ export function createNextButton() {
   nextButton.classList.add("button", "button--next");
   nextButton.setAttribute("data-js", "button-next");
   nextButton.textContent = "next";
+  nextButton.addEventListener("click", () => {
+    cardContainer.innerHTML = "";
+    page++;
+    if (page > maxPage) {
+      page = maxPage;
+    }
+    fetchCharacters();
+  });
   return nextButton;
 }
 
 export function createPrevButton() {
-    const prevButton = document.createElement("button");
-    prevButton.classList.add("button", "button--prev");
-    prevButton.setAttribute("data-js", "button-prev");
-    prevButton.textContent = "previous";
-    return prevButton;
-  }
-
+  const prevButton = document.createElement("button");
+  prevButton.classList.add("button", "button--prev");
+  prevButton.setAttribute("data-js", "button-prev");
+  prevButton.textContent = "previous";
+  prevButton.addEventListener("click", () => {
+    cardContainer.innerHTML = "";
+    page--;
+    if (page < 1) {
+      page = 1;
+    }
+    fetchCharacters();
+  });
+  return prevButton;
+}
