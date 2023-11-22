052c51014b742d1a4bc78e4053b8537b5f82c148
uetrozi <charlotte_keuer@posteo.de>
2023-11-22 16:11:33 +0100

    fix dynamic values of innerhtml og createCardFunction

--- components/card/card.js
+++ components/card/card.js
@@ -1,25 +1,25 @@
 export function createCharacterCard() {
   const characterCard = document.createElement("li");
   characterCard.classList.add("card");
-  characterCard.innerHTML = `
-    <div class="card__image-container">
-      <img
-        class="card__image"
-        src="${CharacterPictureSrc}"
-        alt="${CharacterName}"
-      />
-      <div class="card__image-gradient"></div>
-    </div>
-    <div class="card__content">
-      <h2 class="card__title">${CharacterName}</h2>
-      <dl class="card__info">
-        <dt class="card__info-title">Status</dt>
-        <dd class="card__info-description">${CharacterStatus}</dd>
-        <dt class="card__info-title">Type</dt>
-        <dd class="card__info-description">${CharacterType}</dd>
-        <dt class="card__info-title">Occurrences</dt>
-        <dd class="card__info-description">${CharacterAge}</dd>
-      </dl>
-    </div>`;
+  characterCard.innerHTML`
+  <div class="card__image-container">
+    <img
+      class="card__image"
+      src="${character.image}"
+      alt="${character.name}"
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${character.name}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${character.status}</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${character.type}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${character.age}</dd>
+    </dl>
+  </div>`;
   return characterCard;
 }
