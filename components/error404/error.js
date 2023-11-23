17623fc87fddff636f2b3a10bdb31a6759000e4a
uetrozi <charlotte_keuer@posteo.de>
2023-11-23 22:02:20 +0100

    fix errormessage output

---
+++ components/error404/error.js
@@ -0,0 +1,16 @@
+export function createErrorMessage() {
+  const errorMessage = document.createElement("div");
+  errorMessage.classList.add("background-img");
+  errorMessage.innerHTML = `
+   
+    <div class="space"></div>
+        <div class="wrapper">
+            <div class="img-wrapper">
+                <span>44</span>
+            </div>
+            <p>The page you are trying to search has been <br> moved to another universe.</p>
+            <a href="./index.html" class="errorbutton">GET ME HOME</a>
+        </div>
+    `;
+  return errorMessage;
+}
